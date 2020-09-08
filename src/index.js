617beb51a6257753a5ae42c33d05b9c2b7435d09
Armando <armandez09651@yahoo.com>
2020-09-08 16:59:13 -0500

    Finally got SOMETHING to work
    After about an hour of playing with it, I got something to work. I learned that the return will not work if you try rendering a component without having it within a vanilla html tag

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Nav(props){
+  return(
+    <h2>testing Ollie</h2>
+  );
+}
 
+function App(props) {
+  return (
+
+      <header>Welcome {props.title}
+              <Nav />
+      </header>
+
+  );
+}
+
+ReactDOM.render(<App title = "Ollie"/>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
